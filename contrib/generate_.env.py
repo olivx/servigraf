a82b3126939859111c8293b7cc15879007ce1555
olivx <oliveiravicente.net@gmail.com>
2017-09-03 17:22:30 -0300

    add psql confg

--- contrib/generate_.env.py
+++ contrib/generate_.env.py
@@ -19,6 +19,12 @@ DEBUG=True
 ALLOWED_HOSTS=127.0.0.1, .localhost
 
 #DATABASE_URL=postgres://USER:PASSWORD@HOST:PORT/NAME
+DB_NAME=my_db
+DB_HOST=127.0.0.1
+DB_PORT=5432
+DB_USER=my_user
+DB_PASSWORD=my_password
+
 
 EMAIL_BACKEND=django.core.mail.backends.console.EmailBackend
 DEFAULT_FROM_EMAIL=smtp.teste.com
