6c8b81c3013dbd6e6db6df4fa6f935b413311830
olivx <oliveiravicente.net@gmail.com>
2018-01-13 22:00:20 -0200

    projet admin melhorado

--- project/admin.py
+++ project/admin.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
 from django.contrib import admin
 
 # Register your models here.
@@ -6,9 +8,10 @@ from project.models import Projects, ProjectServices
 
 @admin.register(Projects)
 class ProjectsAdmin(admin.ModelAdmin):
-
+    list_filter = ['active']
     search_fields = ['name', 'desc']
-    fields = ['name', 'desc']
+    list_display = ['name' , 'user' , 'created' , 'updated']
+    fields = ['name', 'desc', 'active' , 'user', 'created']
 
 @admin.register(ProjectServices)
 class ProjectServices(admin.ModelAdmin):
