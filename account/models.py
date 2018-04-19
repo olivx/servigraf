12232cb0f6f5aba550bd2b2b239b158d3b5b8f71
Thiago Oliveira <oliveiravicente.net@gmail.com>
2018-04-19 16:24:08 -0300

    crinado profile e testando

--- account/models.py
+++ account/models.py
@@ -1,19 +1,44 @@
-# from django.db import models
-#
-#
-# # Create your models here.
-# class PasswordReset(models.Model):
-#
-#     user = models.ForeignKey('User', related_name='users')
-#     created = models.DateTimeField(auto_now_add=True, auto_now=False)
-#     expired = models.DateTimeField(null=True, blank=True)
-#     hash =  models.TextField()
-#     ativo = models.NullBooleanField()
-#
-#     def __str__(self):
-#         return self.user.username
-#
-#     class Meta:
-#         ordering = ['-id']
-#         verbose_name = 'Password Reset'
-#         verbose_name_plural = 'Passwords  Reset'
+from django.db import models
+from django.contrib.auth.models import User
+from django.utils.http import urlsafe_base64_encode, force_bytes
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+# Create your models here.
+def path_avatar(instance, filename):
+    path ='avatar/'
+    ext = filename.split('.')[-1]
+    file_name  = '%s.%s' % (urlsafe_base64_encode(force_bytes(instance.user.pk)), ext)
+    return os.path.join(path, file_name)
+
+class Profile(models.Model):
+
+    CLIENT_USER = 1
+    NORMAL_USER = 2
+    ADMIN_USER = 3
+    KINDS =[
+        (CLIENT_USER, 'Cliente'),
+        (NORMAL_USER, 'Comum'),
+        (ADMIN_USER, 'Admin')
+    ]
+
+    user =  models.OneToOneField(User, primary_key=True)
+    type = models.PositiveIntegerField('Tipo', choices=KINDS, default=NORMAL_USER)
+    full_name =  models.CharField('Nome Completo', max_length=255,blank=True, null=True)
+    avatar =  models.ImageField('Avatar', upload_to=path_avatar, blank=True, null=True)
+    birdayth = models.DateField(null=True, blank=True)
+
+
+    def __str__(sefl):
+        return self.full_name
+
+    class Meta:
+        verbose_name =  'Profile'
+        verbose_name_plural = 'Profiles'
+
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    profile, created = Profile.objects.get_or_create(user=instance)
+    profile.full_name = instance.get_full_name()
+    profile.save()
--- account/tests.py
+++ account/tests.py
@@ -1,3 +1,26 @@
 from django.test import TestCase
+from model_mommy import mommy
+from django.contrib.auth.models import User
 
+from account.models import Profile
 # Create your tests here.
+class TestModelProfile(TestCase):
+
+    def setUp(self):
+        self.user = mommy.make(User, first_name='thiago', last_name='oliveira')
+
+    def test_create_profile(self):
+        ''' test if exists Profile '''
+        self.assertEqual(1, Profile.objects.count())
+
+    def test_profile_full_name(self):
+        ''' teste if full name is thiago oliveira '''
+        self.assertEqual(self.user.get_full_name() , self.user.profile.full_name)
+
+    def test_kind(self):
+        ''' test if kind default user is normal user '''
+        self.assertEqual(self.user.profile.type, Profile.NORMAL_USER)
+
+    def test_birdayth_can_be_none(self):
+        ''' test if date birdayth is None'''
+        self.assertIsNone(self.user.profile.birdayth)
